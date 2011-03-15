51e1281d5ee1ac7af48ab69324212a5f6289d030
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-03-15 09:50:49 +0100

    generated simple maven archetype

---
+++ buildergen/src/main/java/info/piwai/buildergen/App.java
@@ -0,0 +1,13 @@
+package info.piwai.buildergen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
