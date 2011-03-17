189d928f24988a57dabade157cfe94a304d0cb6d
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-03-17 10:08:05 +0100

    removing useless stuff

--- buildergenexample/src/main/java/info/piwai/buildergen/bean/SimpleBean.java
+++ buildergenexample/src/main/java/info/piwai/buildergen/bean/SimpleBean.java
@@ -15,18 +15,8 @@
  */
 package info.piwai.buildergen.bean;
 
-import info.piwai.buildergen.api.Build;
 import info.piwai.buildergen.api.Buildable;
 
 @Buildable
 public class SimpleBean {
-	
-	protected SimpleBean() {
-		
-	}
-	
-	@Build
-	protected SimpleBean(int test) {
-	}
-
 }
